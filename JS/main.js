77c331f0e79da6460305288ae12ca2689d46888c
jaredvel <jaredvelazquez12@gmail.com>
2022-03-07 22:22:59 -0500

    js

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+console.log('INSERTA TUS DATOS')
+
+var nom = prompt('Ingresa tu nombre')
+var ape = prompt('Ingresa tu apellido')
+var tel = prompt('Ingresa tu telefono')
+var ciu = prompt('Ingresa tu ciudad')
+
+console.log(nom + ' ' + ape + ' vive en ' + ciu + ' y su celular es ' + tel)
+
+var edad = prompt('Ahora, dime tu año de nacimiento y te diré tu edad actual')
+var edad = 2022 - edad
+
+console.log('Tu edad actual es de ' + edad + ' años.')
